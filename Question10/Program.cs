c322af67ff6b60cda805c8353fbedbf5a97c13fe
Birkan Karaer <karaermustafabirkan@gmail.com>
2024-05-07 20:30:52 +0300

    new questions added

---
+++ Question10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Question10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pozitif sayılarda bölme işlemini çıkarma kullanarak yapan uygulamayı yazınız. 
+            Console.WriteLine("bölünecek sayıyı giriniz");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("bölen sayıyı giriniz");
+            int y = int.Parse(Console.ReadLine());
+            int sonuc = x;
+            int sayac = 0;
+            while (sonuc > 0)
+            {
+                sonuc -= y;  
+                sayac++;
+            }
+            Console.WriteLine(sayac);
+
+        }
+    }
+}
--- Question9/Program.cs
+++ Question9/Program.cs
@@ -4,10 +4,60 @@
     {
         static void Main(string[] args)
         {
+            //Solution1();
+
+            //Solution();
+
+            //pozitif sayılarda çarpma işlemini toplama kullanarak bulan uygulamayı yazınız, recursive ile.
+
+            //Solution3();
+
+            Console.WriteLine("lütfen birinci sayıyı giriniz");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("lütfen ikinci sayıyı giriniz");
+            int sayi2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Toplama(sayi1, sayi2));
+
+        }
+
+        private static void Solution3()
+        {
+            Console.WriteLine("lütfen birinci sayıyı giriniz");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("lütfen ikinci sayıyı giriniz");
+            int sayi2 = int.Parse(Console.ReadLine());
+            int sonuc = 0;
+            int sayac = sayi2;
+            do
+            {
+                sonuc += sayi1;  //3,6,9,12,15
+                sayac--;         //4,3,2,1,0
+            } while (sayac > 0);
+            Console.WriteLine($"{sayi1} X {sayi2} = {sonuc}");
+        }
+
+        private static void Solution()
+        {
+            Console.WriteLine("bir sayı giriniz");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("bir sayı daha giriniz");
+            int sayi2 = int.Parse(Console.ReadLine());
+            int sonuc = 0;
+            int sayac = sayi2;
+            while (sayac > 0)
+            {
+                sonuc += sayi1;
+                sayac--;
+            }
+            Console.WriteLine($"{sayi1} X  {sayi2} = {sonuc}");
+        }
+
+        private static void Solution1()
+        {
             Console.WriteLine("lütfen birinci sayıyı giriniz");
             int s1 = int.Parse(Console.ReadLine());
             Console.WriteLine("lütfen ikinci sayıyı giriniz");
-            int s2 = int.Parse(Console.ReadLine()); 
+            int s2 = int.Parse(Console.ReadLine());
             int sonuc = 0;
             while (0 < s2)
             {
@@ -15,6 +65,13 @@
                 s2--;
             }
             Console.WriteLine(sonuc);
+        }
+
+        static int Toplama(int sayi1, int sayi2)
+        {
+            if (sayi2 > 1)
+                return sayi1 + Toplama(sayi1, --sayi2);
+            return sayi1;
         }
     }
 }
